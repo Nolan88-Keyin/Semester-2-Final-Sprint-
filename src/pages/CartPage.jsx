0d85787fe1a2f02aaf2cc715d00f9cc1480a2fdb
Nolan88-Keyin <nolan.butt@keyin.com>
2026-04-24 15:33:32 -0230

    Margins

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -3,7 +3,7 @@ import Cart from "../components/Cart";
 function CartPage({ cart, increaseQuantity, decreaseQuantity, removeFromCart }) {
   return (
     <div>
-      <h1 className="page-title">Your Cart</h1>
+      <h1 className="page-title" style={{ marginLeft: '0.8rem', marginTop: '1rem' }}>Your Cart</h1>
 
       <Cart
         cart={cart}
